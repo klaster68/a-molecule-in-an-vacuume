8da5eeb542111f1a32858a9b840594f7b05b0b2d
Vyacheslav <nonamesnbb@yandex.ru>
2024-07-22 16:55:21 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,68 @@
-const arrObj = {
-    a: 'a',
-    b: 'b'
+// Данные со страницы
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+// Данные на события
+let timerID;
+let flag = true;
+
+// Данные для молекулы
+const Pi = 3.14;
+const R = 2, V = 1, N = 100;
+let x, y, Vx, Vy, angle;
+
+x = Math.floor(Math.random() * (canvas.width - R)) + R;
+y = Math.floor(Math.random() * (canvas.height - R)) + R;
+angle = Math.random() * (360 - 0) * Pi / 180;
+Vx = Math.round(V * Math.sin(angle));
+Vy = Math.round(V * Math.cos(angle));
+
+function getRadians(degrees) {
+    return (Math.PI / 180) * degrees;
 }
 
-console.log(arrObj);
+function drawMolecule() {
+    ctx.beginPath();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.arc(x + Vx, y + Vy, R, 0, getRadians(360));
 
-const c = 'c';
-console.log(c);
-// arrObj[c] = '1234';
-console.log(arrObj[c]);
-console.log(c);
+    x = x + Vx;
+    y = y + Vy;
+
+    // Проверка выхода за границы
+    if (x > canvas.width - R) {
+        x = canvas.width - R;
+        Vx = -Vx;
+    }
+    if (x < R) {
+        x = R;
+        Vx = -Vx;
+    }
+    if (y > canvas.height - R) {
+        y = canvas.width - R;
+        Vy = -Vy;
+    }
+    if (y < R) {
+        y = R;
+        Vy = -Vy;
+    }
+
+    ctx.stroke();
+}
+
+// Кнопки старт-стоп
+start.addEventListener('click', () => {
+    if(flag){
+        timerID = setInterval(drawMolecule, 10);
+        flag = false;
+    }
+});
+
+stop.addEventListener('click', () => {
+    if(!flag){
+        clearInterval(timerID);
+        flag = true;
+    }
+});
